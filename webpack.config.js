18ec444f30fc7a35831970e0b78c0aa331a21ad5
Kostya Bekker <bekker_kostya@ukr.net>
2020-01-26 15:26:15 +0200

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -13,20 +13,20 @@ module.exports = {
   },
   module: {
     rules: [{
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-        loader: 'url-loader?limit=100000'
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader'
       }
+    },
+    {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    },
+    {
+      test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+      loader: 'url-loader?limit=100000'
+    }
     ]
   },
   resolve: {
@@ -36,10 +36,7 @@ module.exports = {
     historyApiFallback: true,
     port: 3000,
     open: true,
-    proxy: {
-      '/api': 'http://localhost:8080'
-    }
-  },
+
   plugins: [
     new CleanWebpackPlugin([outputDirectory]),
     new HtmlWebpackPlugin({
